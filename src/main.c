eb3a0193f668323a8b9181fcfe013109af6ef307
JnKDog <530661401@qq.com>
2021-09-07 21:45:50 +0100

    add cmake to replace make

--- src/main.c
+++ src/main.c
@@ -2,8 +2,10 @@
 #include <string.h>
 
 #include "parse.h"
+// #include "table.h"
 
 int main(int argc, char* argv[]) {
+    // Table* table = new_table();
     InputBuffer* input_buffer = input_buffer_init();
     while (true) {
         print_prompt();
